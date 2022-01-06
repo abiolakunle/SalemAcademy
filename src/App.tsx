7bf1d8aa4fe5702a742c9569f0e57ee9ab4f9116
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2022-01-06 11:37:31 +0100

    loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,25 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppLayout from './router/AppLayout';
 import { BrowserRouter } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+  const [isLoading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const el = document.querySelector(".loader-content");
+
+    if (el) {
+      el.remove();
+      setLoading(!isLoading);
+    }
+
+
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
 
 
   return (
